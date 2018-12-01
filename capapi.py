4247ea3a2a17c5af18e013a09115fa936fa84f9f
ebwydra <ebwydra@gmail.com>
2018-11-30 19:27:13 -0500

    added tables to db: States and DistrictCourts

--- capapi.py
+++ capapi.py
@@ -5,9 +5,10 @@ import csv
 import sqlite3 as sqlite
 from secrets import CAPAPI_TOKEN
 
-''' Caching setup '''
-
+DBNAME = 'law.db'
+STATESCSV = 'state_table.csv'
 CACHE_FNAME = "cache.json"
+
 try:
     f = open(CACHE_FNAME, 'r')
     f_contents = f.read()
@@ -82,3 +83,127 @@ first_url = "https://api.case.law/v1/cases/?full_case=true&jurisdiction=us"
 # print(data['next']) # https://api.case.law/v1/cases/?cursor=cD0xNzk1LTAxLTAx&full_case=true&jurisdiction=us
 # print(data['previous'])
 # print(len(data['results']))
+
+''' Create database '''
+
+def create_db():
+
+    conn = sqlite.connect(DBNAME)
+    cur = conn.cursor()
+
+    ''' Drop existing tables '''
+
+    statement = "DROP TABLE IF EXISTS Cases"
+    cur.execute(statement)
+    statement = "DROP TABLE IF EXISTS DistrictCourts"
+    cur.execute(statement)
+    # statement = "DROP TABLE IF EXISTS CircuitCourts"
+    # cur.execute(statement)
+    statement = "DROP TABLE IF EXISTS States"
+    cur.execute(statement)
+
+    conn.commit()
+
+    ''' Create tables '''
+
+    # Create States table
+    statement = '''
+    CREATE TABLE States (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT,
+    Abbr TEXT,
+	AssocPress TEXT,
+	CensusRegionName TEXT,
+	CensusDivisionName TEXT,
+	CircuitCourt TEXT
+    );
+    '''
+    cur.execute(statement)
+
+    # Create DistrictCourts table
+    statement = '''
+    CREATE TABLE DistrictCourts (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    StateId INTEGER REFERENCES States(Id),
+    CourtName TEXT,
+    Citation TEXT,
+    CircuitCourt TEXT,
+    Established INTEGER,
+    NumJudges INTEGER
+    );
+    '''
+    cur.execute(statement)
+
+    # Create Cases table
+    statement = '''
+    CREATE TABLE Cases (
+    Id INTEGER PRIMARY KEY,
+    Name TEXT,
+    NameAbbr TEXT,
+    DecisionDate INTEGER,
+    CourtId INTEGER REFERENCES DistrictCourts(Id),
+    CaseBody TEXT
+    );
+    '''
+    cur.execute(statement)
+
+    conn.commit()
+
+    ''' Load data into tables '''
+
+    # States table - from csv
+    with open(STATESCSV, encoding = 'utf-8') as states_data:
+        list_of_tuples = []
+
+        csv_reader = csv.reader(states_data)
+        for row in csv_reader:
+            name = row[1]
+            abbr = row[2]
+            ap = row[10]
+            region = row[13]
+            division = row[15]
+            circuit = row[16]
+            tup = (name,abbr,ap,region,division,circuit)
+            list_of_tuples.append(tup)
+
+        for tup in list_of_tuples[1:]:
+            statement = '''
+            INSERT INTO States (Name, Abbr, AssocPress, CensusRegionName, CensusDivisionName, CircuitCourt)
+            VALUES (?,?,?,?,?,?)
+            '''
+            cur.execute(statement, tup)
+            conn.commit()
+
+    # DistrictCourts table
+    courts_list = get_courts_data() # list of tuples
+    for court in courts_list:
+        # Get State ID -- court[0]
+        statement = '''
+        SELECT Id
+        FROM States
+        WHERE Name LIKE \"{}\"
+        '''.format(court[0])
+        cur.execute(statement)
+        result = cur.fetchone()
+        try:
+            id = result[0]
+        except:
+            id = None
+        # print("{} {}".format(id, court[1]))
+        court_l = list(court)
+        court_l.append(id)
+        court_l.remove(court_l[0]) # get rid of state name
+        new_tup = tuple(court_l)
+        # print(new_tup)
+
+        # ('District of Wyoming', 'D. Wyo.', '10th', '1890', '3', 50)
+
+        statement = '''
+        INSERT INTO DistrictCourts (CourtName, Citation, CircuitCourt, Established, NumJudges, StateId)
+        VALUES (?, ?, ?, ?, ?, ?)
+        '''
+        cur.execute(statement, new_tup)
+        conn.commit()
+
+
+create_db()
